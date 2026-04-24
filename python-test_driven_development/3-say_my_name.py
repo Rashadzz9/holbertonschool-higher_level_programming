94c31cef6a1901867cf81aa9a55ff9cf3f93a34c
Rashadzz9 <zbiyevrashad@gmail.com>
2026-04-24 11:40:37 +0000

    task

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Bu modul say_my_name funksiyasini ehtiva edir."""
+
+def say_my_name(first_name, last_name=""):
+    """Ad və soyadı çap edən funksiya."""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
