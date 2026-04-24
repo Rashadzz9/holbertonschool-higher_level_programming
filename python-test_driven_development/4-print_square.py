a9b9b7a7e066e49d7ccf4bdedf0eb443215e77ca
Rashadzz9 <zbiyevrashad@gmail.com>
2026-04-24 12:06:49 +0000

    task

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-def print_square(size):
-    """burada iste bir eded olur ve bir #simvolu onu 4 e vuranda 4 dene eyni simvol yansai olacag onu isteyir bizden"""
+"""burada iste bir eded olur ve bir #simvolu onu 4 e vuranda 4 dene eyni simvol yansai olacag onu isteyir bizden"""
 
-   if  isinstance(size, float) and size < 0:
+
+def print_square(size):
+    """funksiya yaziyoruze"""
+    if not isinstance(size, int):
+        if isinstance(size, float) and size < 0:
+            raise TypeError("size must be an integer")
         raise TypeError("size must be an integer")
-   if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-   if size < 0:
-       raise TypeError("size must be >= 0")
-   for i in range(size):
-       print("#" * size)
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
