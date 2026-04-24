e2aee1147d1a3273de8a15da5f4e03cfc3e83a04
Rashadzz9 <zbiyevrashad@gmail.com>
2026-04-24 12:15:01 +0000

    task

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module provides a function that indents text.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    special_chars = [".", "?", ":"]
+    
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] in special_chars or text[c] == "\n":
+            if text[c] in special_chars:
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
