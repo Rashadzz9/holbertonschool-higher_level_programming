6e1b38910701c3656c7394f31f038ba1778243f6
Rashadzz9 <zbiyevrashad@gmail.com>
2026-04-27 12:28:38 +0000

    task

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,47 +2,28 @@
 class Square:
     """This class defines a square."""
 
-    def __init__(self, size=0):
-        """Initializes a new Square instance.
 
-        Args:
-            size (int): The size of the square's sides. Defaults to 0.
-        """
-        # This will automatically call the setter method below
+    def __init__(self, size=0):
+        """Initializes a new Square instance"""
+
         self.size = size
 
     @property
     def size(self):
-        """Retrieves the size of the square.
-
-        Returns:
-            int: The current size of the square.
-        """
+        """Retrieves the size of the squar"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size of the square with validation.
-
-        Args:
-            value (int): The new size to be set.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        """
+        """Sets the size of the square with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = value
 
     def area(self):
-        """Calculates and returns the current square area.
-
-        Returns:
-            int: The area of the square.
-        """
+        """Calculates and returns the current square."""
         return self.__size ** 2
 
