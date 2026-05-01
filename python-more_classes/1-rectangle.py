46c03ad51e64e1c2883c2bbcd84a719513b06a4c
Rashadzz9 <zbiyevrashad@gmail.com>
2026-05-01 10:37:23 +0000

    task

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,37 +3,37 @@
 
 
 class Rectangle:
-    """Defines a  this file is a class"""
+    """Defines a rectangle class"""
 
     def __init__(self, width=0, height=0):
-       """Define edirik hight ve width onlari tanidirig"""
-       self.width = width
-       self.height = height
+        """Initializes the rectangle with width and height"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """width deyerini __ olarag retireve"""
+        """Retrieves the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """errorlari basa salacayig ki bunu ver"""
+        """Sets the width with validation"""
         if not isinstance(value, int):
-           raise TypeError("width must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-           raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """eyni qayda ile height edirik"""
+        """Retrieves the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """burada ise errorlari veririk"""
+        """Sets the height with validation"""
         if not isinstance(value, int):
-           raise TypeError("height must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-           raise ValueError("height must be >= 0")
-           self.__height = value
+            raise ValueError("height must be >= 0")
+        self.__height = value
