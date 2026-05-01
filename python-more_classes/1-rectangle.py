9be02680d8f7995d1648ad1be67a3a62d3bd9ddf
Rashadzz9 <zbiyevrashad@gmail.com>
2026-05-01 10:18:50 +0000

    task

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
-"""Module that defines a Rectangle class with width and height."""
-
+"""This code defines width and height"""
 
 class Rectangle:
-    """Class that represents a rectangle."""
+    """Defines a  this file is a class"""
 
     def __init__(self, width=0, height=0):
-        """Initialize the rectangle.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        self.width = width
-        self.height = height
+       """Define edirik hight ve width onlari tanidirig"""
+       self.width = width
+       sefl.height = height
 
     @property
     def width(self):
-        """Get or set the width of the rectangle."""
+        """width deyerini __ olarag retireve"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """errorlari basa salacayig ki bunu ver"""
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+           raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+           raise TypeError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """Get or set the height of the rectangle."""
+        """eyni qayda ile height edirik"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """burada ise errorlari veririk"""
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+           raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+           raise ValueError("height must be >= 0")
+           self.height = value
