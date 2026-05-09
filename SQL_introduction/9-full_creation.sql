7178fddeffec766f1df91148ad0eed78eaeb74c2
Rashadzz9 <zbiyevrashad@gmail.com>
2026-05-09 11:29:57 +0000

    task

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- creates second table which have multiple rows
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES (1, 'JOHN', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'ALEX', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'BOB', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'GEORGE', 8);
