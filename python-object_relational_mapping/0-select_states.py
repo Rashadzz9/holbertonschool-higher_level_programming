3a15db0c430bee4d6e455c328d2e2da9bbb9b10c
Rashadzz9 <zbiyevrashad@gmail.com>
2026-05-12 15:11:10 +0200

    task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python3
-"""This script lists all states from the database hbtn_0e_0_usa."""
+"""
+This script lists all states from the database hbtn_0e_0_usa.
+It takes 3 arguments: mysql username, mysql password and database name.
+"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-         user_name = sys.argv[1]
-         password = sys.argv[2]
-         db_name = sys.argv[3]
+    # Get arguments from command line
+    # sys.argv[0] is the script name, so we start from 1
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-         db = MySQLdb.connect(
-             host="localhost",
-             port=3306,
-             user=user_name,
-             passwd=password,
-             db=db_name
-         )
+    # Connect to MySQL server
+    # We use localhost and port 3306 as per requirements
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user_name,
+        passwd=password,
+        db=db_name
+    )
 
-         cur = db.cursor()
+    # Create a cursor to execute SQL queries
+    cur = db.cursor()
 
-         cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # Execute the query and sort by states.id
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-         rows = cur.fetchall()
+    # Fetch all results
+    rows = cur.fetchall()
 
-         for row in rows:
-             print(row)
+    # Print results in the specified format
+    for row in rows:
+        print(row)
 
-         cur.close()
-         db.close()
+    # Close all connections (Clean up)
+    cur.close()
+    db.close()
