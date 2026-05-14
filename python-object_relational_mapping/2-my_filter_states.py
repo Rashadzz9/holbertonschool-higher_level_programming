d3d27849e11797564cb5277c34ad41221e571cf4
Rashadzz9 <zbiyevrashad@gmail.com>
2026-05-14 14:23:22 +0000

    task

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """This script takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument."""
+
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
