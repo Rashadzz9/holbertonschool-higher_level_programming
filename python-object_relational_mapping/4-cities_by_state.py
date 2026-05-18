460976d64ffaf7d17bc832b87aade9dd4101c129
Rashadzz9 <zbiyevrashad@gmail.com>
2026-05-18 13:50:25 +0000

    task

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
 This script lists all cities from the database hbtn_0e_4_usa.
-It uses a JOIN to fetch state names for each city.
+It connects to a MySQL server and uses a JOIN operation.
 """
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    # 3 arqument qəbul edirik: user, passwd, db
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -19,13 +18,10 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    # Yalnız bircə dəfə execute() işlədirik və JOIN sorğusunu yazırıq
-    query = """
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        ORDER BY cities.id ASC
-    """
+    # Sorğunu tək sətirdə yazırıq ki, gizli boşluq xətaları yaranmasın
+    query = "SELECT cities.id, cities.name, states.name FROM cities JOIN " \
+            "states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    
     cursor.execute(query)
 
     rows = cursor.fetchall()
