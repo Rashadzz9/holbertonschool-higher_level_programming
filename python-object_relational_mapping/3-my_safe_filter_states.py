2ece8c9993868b5ab1772a162b699ec57e86659e
Rashadzz9 <zbiyevrashad@gmail.com>
2026-05-20 08:49:14 +0000

    task

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Wait, do you remember the previous task make a new"""
-
+"""
+This script takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument."""
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
@@ -11,11 +13,13 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
-   )
+    )
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",(sys.argv[4],))
+    # Sətri 79 simvoldan aşağı saxlamaq üçün iki hissəyə böldük
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (sys.argv[4],))
 
     rows = cursor.fetchall()
 
